3498cb403378e958495f80c42b63c4a68303f22a
srivarshini1899 <70571200+srivarshini1899@users.noreply.github.com>
2020-09-09 16:24:03 +0200

    Add files via upload

---
+++ Eureka/src/test/java/com/bank/team7/eureka/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.team7.eureka.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
